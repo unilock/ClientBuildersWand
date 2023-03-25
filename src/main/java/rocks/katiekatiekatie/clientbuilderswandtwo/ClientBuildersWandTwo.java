00b8fc4dee8b0922c2d68043a13bd9ef011ae11f
unilock <unilock@fennet.rentals>
2023-03-25 19:00:20 -0400

    init
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/rocks/katiekatiekatie/clientbuilderswandtwo/ClientBuildersWandTwo.java
@@ -0,0 +1,13 @@
+package rocks.katiekatiekatie.clientbuilderswandtwo;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("clientbuilderswandtwo")
+public class ClientBuildersWandTwo {
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public ClientBuildersWandTwo() {
+    }
+}
